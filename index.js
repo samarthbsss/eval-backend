49f1a698ff336f8f8fb53a087f678b66e4f44a68
Samarth <samarthbsacharya@gmail.com>
2023-07-01 02:01:59 +0530

    change

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ const server = jsonServer.create()
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-server.use(
-  cors({
-    origin: true,
-    credentials: true,
-    preflightContinue: false,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  })
-);
-server.options("*", cors());
+// server.use(
+//   cors({
+//     origin: true,
+//     credentials: true,
+//     preflightContinue: false,
+//     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
+//   })
+// );
+// server.options("*", cors());
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', 'http://localhost:3000')
