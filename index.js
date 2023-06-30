fc2d17471e528844888320a80d01dfa13ec6d66b
Samarth <samarthbsacharya@gmail.com>
2023-07-01 01:40:56 +0530

    change

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
 server.use(cors({
-  origin:'*',
+  origin:"*",
 }));
 
 server.use(jsonServer.bodyParser)
