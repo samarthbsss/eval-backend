6457e5b1d95f6533b6251930005639e6c782f213
Samarth <samarthbsacharya@gmail.com>
2023-06-30 23:52:28 +0530

    work

--- src/Redux/store.js
+++
@@ -1,7 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import thunk from 'redux-thunk';
-import rootreducer from './reducer';
-
-const store= createStore(rootreducer, applyMiddleware(thunk));
-
-export default store;
